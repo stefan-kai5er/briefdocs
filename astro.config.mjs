b6dd18b982e81bc1a402d50647853f49cc8f3632
stefan-kai5er <git@stefankaiser.at>
2025-09-16 18:37:43 +0200

    Add startlight-sidbar-topics plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,40 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightSidebarTopics from "starlight-sidebar-topics";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      plugins: [
+        starlightSidebarTopics([
+          {
+            label: "Guides",
+            link: "/guides/example",
+            icon: "open-book",
+            items: [
+              { label: "Reference", autogenerate: { directory: "guides" } },
+            ],
+          },
+          {
+            label: "Reference",
+            link: "/reference/example",
+            icon: "information",
+            items: [
+              { label: "Reference", autogenerate: { directory: "reference" } },
+            ],
+          },
+        ]),
+      ],
+    }),
+  ],
 });
