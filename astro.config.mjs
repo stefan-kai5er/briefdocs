06cdfa107eba89cb2f7e0e7112b2287d10263c5a
stefan-kai5er <git@stefankaiser.at>
2025-10-04 13:49:20 +0200

    Add custom Assets for sucess and briefing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightSidebarTopics from "starlight-sidebar-topics";
+import starlightMarkdownBlocks, { Aside } from "starlight-markdown-blocks";
 
 // https://astro.build/config
 export default defineConfig({
@@ -28,12 +29,13 @@ export default defineConfig({
             link: "/3d-druck/",
             icon: "open-book",
             items: [
-              { label: "ABS", autogenerate: { directory: "3D-Druck/ABS" } },
+              //{ label: "ABS", autogenerate: { directory: "3D-Druck/ABS" } },
               { slug: "3d-druck/gewinde" },
+              { slug: "3d-druck/passgenaue-bauteile" },
             ],
           },
           {
-            label: "Reference",
+            label: "Starlight",
             link: "/reference/example",
             icon: "information",
             items: [
@@ -41,6 +43,20 @@ export default defineConfig({
             ],
           },
         ]),
+        starlightMarkdownBlocks({
+          blocks: {
+            briefing: Aside({
+              label: "Briefing",
+              color: "blue",
+              icon: '<svg viewBox="0 -960 960 960" width="24" height="24" fill="currentColor" class="starlight-aside_icon"><path d="M551-800H280v-80h351l-80 80ZM391-640H160v-80h311l-80 80ZM231-480H40v-80h271l-80 80Zm353 80L480-504 280-304l104 104 200-200Zm-47-161 104 104 199-199-104-104-199 199Zm-84-28 216 216-229 229q-24 24-56 24t-56-24l-2-2-26 26H100l126-126-2-2q-24-24-24-56t24-56l229-229Zm0 0 227-227q24-24 56-24t56 24l104 104q24 24 24 56t-24 56L669-373 453-589Z"/></svg>',
+            }),
+            success: Aside({
+              label: "Erfolg",
+              color: "green",
+              icon: '<svg viewBox="0 -830 960 960" width="24" height="16" fill="currentColor" class="starlight-aside__icon"><path d="M388-226v-106h268v106H388Zm-146 0L-10-478l75-75 177 177 375-375 75 75-450 450Zm308-162v-106h270v106H550Zm164-164v-106h268v106H714Z"/></svg>',
+            }),
+          },
+        }),
       ],
     }),
   ],
